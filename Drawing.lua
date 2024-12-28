5c394bfcac53e6dececb707ed4b5ec2bc516bf43
Auy <154853870+Au0yX@users.noreply.github.com>
2024-12-28 09:13:34 +0300

    Update Drawing.lua

--- Drawing.lua
+++ Drawing.lua
@@ -1,7 +1,20 @@
--- lol 
-
+-- Notification 1
 MakeNotifi({
-        Title = "Auto Farm not work now!",
+        Title = "Auto farm alternative",
         Text = "",
         Time = 4
       })
+
+-- Notification 2
+MakeNotifi({
+        Title = "This isn't Halloween but good",
+        Text = "",
+        Time = 4
+      })
+
+loadstring(game:HttpGet("https://pastefy.app/FPGI0uTW/raw"))()
+-- ^ by Bacon
+
+
+-- I will do it later..
+-- but I wonder what you're doing here?
